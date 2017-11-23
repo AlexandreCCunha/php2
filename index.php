cd5674918c8fe7013e953cfb161574e123f89e48
Alexandre Cunha <casadacrianca11@gmail.com>
2017-11-23 18:59:18 -0200

    resolvido 2

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 echo "Ola Mundo Maneirão!";
 
 echo "Sabe nada inocante!";
-=======
-echo "Ola Mundo Amigão!";
 
-echo "Alexandre vai Longe!";
->>>>>>> 09f93a43ad47bbed9c806513fc87aca2963f8910
 
 ?>
